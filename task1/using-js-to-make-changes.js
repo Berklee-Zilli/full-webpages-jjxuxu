a30008477619401462abaecec5d1a1f1e0a6130f
Juliann Xu <juliannxu@Julianns-MacBook-Pro.local>
2024-11-19 19:22:57 -0500

    Level 1 complete

---
+++ task1/using-js-to-make-changes.js
@@ -0,0 +1,5 @@
+document.title = "Task 1 by Juliann";
+
+document.querySelector("h1").innerHTML = "TASK 1";
+document.querySelector("p").innerHTML = "Juliann is always sleepy";
+document.querySelector("h2").innerHTML = "List of Things I Like";
